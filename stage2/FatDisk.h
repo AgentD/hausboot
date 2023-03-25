44ab2fab2ef73ffc3e4ddf14f4339507fcbc3b4a
David Oberhollenzer <goliath@infraroot.at>
2023-03-25 18:46:14 +0100

    Cleanup: try to stuff the stage2 disk plumbing into a data structure
    Signed-off-by: David Oberhollenzer <goliath@infraroot.at>

---
+++ stage2/FatDisk.h
@@ -0,0 +1,140 @@
+/* SPDX-License-Identifier: ISC */
+/*
+ * FatDisk.h
+ *
+ * Copyright (C) 2023 David Oberhollenzer <goliath@infraroot.at>
+ */
+#ifndef FAT_DISK_H
+#define FAT_DISK_H
+
+#include "BIOS/TextScreen.h"
+#include "BIOS/BiosDisk.h"
+#include "part/MBREntry.h"
+#include "fs/FatSuper.h"
+
+static TextScreen &operator<< (TextScreen &s, CHSPacked chs)
+{
+	s << chs.Cylinder() << "/" << chs.Head() << "/" << chs.Sector();
+	return s;
+}
+
+static TextScreen &operator<< (TextScreen &s,
+			       const BiosDisk::DriveGeometry &geom)
+{
+	s << geom.cylinders << "/" << geom.headsPerCylinder << "/"
+	  << geom.sectorsPerTrack;
+	return s;
+}
+
+class FatDisk {
+public:
+	bool Init(TextScreen &ts, BiosDisk bootDisk,
+		  const MBREntry &part, const FatSuper *fsSuper,
+		  void *&heapPtr) {
+		disk = bootDisk;
+		screen = &ts;
+		partStart = part.StartAddressLBA();
+		super = fsSuper;
+
+		if (!disk.ReadDriveParameters(driveGeometry)) {
+			(*screen) << "Error querying disk geometry!"
+				  << "\r\n";
+			return false;
+		}
+
+		(*screen) << "Drive geometry (C/H/S): "
+			  << driveGeometry << "\r\n";
+
+		currentFatSector = 0xFFFFFFFF;
+		currentDataCluster = 0xFFFFFFFF;
+
+		fatWindow = (uint8_t *)heapPtr;
+		heapPtr = fatWindow + super->BytesPerSector();
+
+		dataWindow = (uint8_t *)heapPtr;
+		heapPtr = dataWindow + BytesPerCluster();
+		return true;
+	}
+
+	bool LoadFatSector(uint32_t index) {
+		if (index >= super->SectorsPerFat()) {
+			*screen << "[BUG] tried to access out-of-bounds "
+				"FAT sector: " << index << " >= "
+			       << super->SectorsPerFat() << "\r\n";
+			return false;
+		}
+
+		if (index == currentFatSector)
+			return true;
+
+		auto lba = partStart + super->ReservedSectors() + index;
+		auto chs = driveGeometry.LBA2CHS(lba);
+
+		if (!disk.LoadSectors(chs, fatWindow, 1)) {
+			*screen << "Error loading disk sector " << lba
+			       << " while accessing FAT index " << index
+			       << "(CHS: " << chs << ")" << "\r\n";
+			return false;
+		}
+
+		currentFatSector = index;
+		return true;
+	}
+
+	bool ReadFatIndex(uint32_t index, uint32_t &out) {
+		auto sector = (index * 4) / super->BytesPerSector();
+		auto offset = (index * 4) % super->BytesPerSector();
+
+		if (!LoadFatSector(sector))
+			return false;
+
+		out = *((uint32_t *)(fatWindow + offset));
+		return true;
+	}
+
+	bool LoadDataCluster(uint32_t index) {
+		if (index < 2) {
+			*screen << "[BUG] tried to access data cluster < 2"
+			       << "\r\n";
+			return false;
+		}
+
+		if (index == currentDataCluster)
+			return true;
+
+		auto sector = partStart + super->ClusterIndex2Sector(index);
+		auto chs = driveGeometry.LBA2CHS(sector);
+
+		if (!disk.LoadSectors(chs, dataWindow,
+				      super->SectorsPerCluster())) {
+			*screen << "Error loading cluster " << index
+			       << " from sector " << sector
+			       << " (CHS: " << chs << ")" << "\r\n";
+			return false;
+		}
+
+		currentDataCluster = index;
+		return true;
+	}
+
+	size_t BytesPerCluster() const {
+		return super->SectorsPerCluster() * super->BytesPerSector();
+	}
+
+	auto RootDirIndex() const {
+		return super->RootDirIndex();
+	}
+
+	uint8_t *fatWindow;
+	uint8_t *dataWindow;
+private:
+	TextScreen *screen;
+	const FatSuper *super;
+	uint32_t currentFatSector;
+	uint32_t currentDataCluster;
+	BiosDisk::DriveGeometry driveGeometry;
+	BiosDisk disk{0};
+	uint32_t partStart;
+};
+
+#endif /* FAT_DISK_H */
--- stage2/stage2.cpp
+++ stage2/stage2.cpp
@@ -8,6 +8,7 @@
 #include "fs/FatDirentLong.h"
 #include "fs/FatDirent.h"
 #include "fs/FatSuper.h"
+#include "FatDisk.h"
 #include "stage2.h"
 
 __attribute__ ((section(".header")))
@@ -17,97 +18,7 @@ static const char *bootConfigName = "BOOT.CFG";
 
 static auto *stage2header = (Stage2Info *)headerBlob;
 static TextScreen screen;
-static BiosDisk::DriveGeometry driveGeometry;
-static auto *fatSuper = (FatSuper *)0x7C00;
-
-static uint8_t *fatWindow;
-static uint8_t *dataWindow;
-static uint32_t currentFatSector;
-static uint32_t currentDataCluster;
-
-static TextScreen &operator<< (TextScreen &s, CHSPacked chs)
-{
-	s << chs.Cylinder() << "/" << chs.Head() << "/" << chs.Sector();
-	return s;
-}
-
-static TextScreen &operator<< (TextScreen &s,
-			       const BiosDisk::DriveGeometry &geom)
-{
-	s << geom.cylinders << "/" << geom.headsPerCylinder << "/"
-	  << geom.sectorsPerTrack;
-	return s;
-}
-
-static bool LoadFatSector(uint32_t index)
-{
-	if (index >= fatSuper->SectorsPerFat()) {
-		screen << "[BUG] tried to access out-of-bounds FAT sector: "
-		       << index << " >= "
-		       << fatSuper->SectorsPerFat() << "\r\n";
-		return false;
-	}
-
-	if (index == currentFatSector)
-		return true;
-
-	auto lba = stage2header->BootMBREntry().StartAddressLBA() + index;
-	lba += fatSuper->ReservedSectors();
-
-	auto chs = driveGeometry.LBA2CHS(lba);
-	auto disk = stage2header->BiosBootDrive();
-
-	if (!disk.LoadSectors(chs, fatWindow, 1)) {
-		screen << "Error loading disk sector " << lba
-		       << " while accessing FAT index " << index
-		       << "(CHS: " << chs << ")" << "\r\n";
-		return false;
-	}
-
-	currentFatSector = index;
-	return true;
-}
-
-static bool ReadFatIndex(uint32_t index, uint32_t &out)
-{
-	auto sector = (index * 4) / fatSuper->BytesPerSector();
-	auto offset = (index * 4) % fatSuper->BytesPerSector();
-
-	if (!LoadFatSector(sector))
-		return false;
-
-	out = *((uint32_t *)(fatWindow + offset));
-	return true;
-}
-
-static bool LoadDataCluster(uint32_t index)
-{
-	if (index < 2) {
-		screen << "[BUG] tried to access data cluster < 2"
-		       << "\r\n";
-		return false;
-	}
-
-	if (index == currentDataCluster)
-		return true;
-
-	auto sector = stage2header->BootMBREntry().StartAddressLBA();
-	sector += fatSuper->ClusterIndex2Sector(index);
-
-	auto chs = driveGeometry.LBA2CHS(sector);
-	auto disk = stage2header->BiosBootDrive();
-
-	if (!disk.LoadSectors(chs, dataWindow,
-			      fatSuper->SectorsPerCluster())) {
-		screen << "Error loading cluster " << index
-		       << " from sector " << sector
-		       << " (CHS: " << chs << ")" << "\r\n";
-		return false;
-	}
-
-	currentDataCluster = index;
-	return true;
-}
+static FatDisk disk;
 
 static bool StrEqual(const char *a, const char *b)
 {
@@ -133,15 +44,14 @@ struct FatFile {
 		do {
 			uint32_t next;
 
-			if (!LoadDataCluster(index))
+			if (!disk.LoadDataCluster(index))
 				return false;
 
-			if (!ReadFatIndex(index, next))
+			if (!disk.ReadFatIndex(index, next))
 				return false;
 
-			auto *entS = (FatDirent *)dataWindow;
-			auto max = (fatSuper->SectorsPerCluster() *
-				    fatSuper->BytesPerSector()) / sizeof(*entS);
+			auto *entS = (FatDirent *)disk.dataWindow;
+			auto max = disk.BytesPerCluster() / sizeof(*entS);
 
 			for (decltype(max) i = 0; i < max; ++i) {
 				if (entS[i].IsLastInList())
@@ -176,18 +86,17 @@ struct FatFile {
 		while (fileSize > 0 && index < 0x0FFFFFF0) {
 			uint32_t next;
 
-			if (!LoadDataCluster(index))
+			if (!disk.LoadDataCluster(index))
 				return false;
 
-			if (!ReadFatIndex(index, next))
+			if (!disk.ReadFatIndex(index, next))
 				return false;
 
-			auto diff = fatSuper->SectorsPerCluster() *
-				fatSuper->BytesPerSector();
+			auto diff = disk.BytesPerCluster();
 			if (diff > fileSize)
 				diff = fileSize;
 
-			screen.WriteCharacters((char *)dataWindow, diff);
+			screen.WriteCharacters((char *)disk.dataWindow, diff);
 
 			fileSize -= diff;
 			index = next;
@@ -201,32 +110,17 @@ void main(void *heapPtr)
 {
 	screen.Reset();
 
-	// allocate sliding windows to read from the disk
-	fatWindow = (uint8_t *)heapPtr;
-	heapPtr = fatWindow + fatSuper->BytesPerSector();
+	auto ret = disk.Init(screen,
+			     stage2header->BiosBootDrive(),
+			     stage2header->BootMBREntry(),
+			     (const FatSuper *)0x7C00, heapPtr);
 
-	dataWindow = (uint8_t *)heapPtr;
-	heapPtr = dataWindow +
-		fatSuper->BytesPerSector() * fatSuper->SectorsPerCluster();
-
-	// Get drive geometry
-	auto disk = stage2header->BiosBootDrive();
-
-	if (!disk.ReadDriveParameters(driveGeometry)) {
-		screen << "Error querying disk geometry!\r\n";
+	if (!ret)
 		goto fail;
-	}
 
-	screen << "Drive geometry (C/H/S): " << driveGeometry << "\r\n";
-
-	// Initialize FAT and cluster sliding windows
-	currentFatSector = 0xFFFFFFFF;
-	currentDataCluster = 0xFFFFFFFF;
-
-	// DUMP
 	{
 		FatFile finfo;
-		finfo.cluster = fatSuper->RootDirIndex();
+		finfo.cluster = disk.RootDirIndex();
 		finfo.size = 0;
 		finfo.flags.Set(FatDirent::Flags::Directory);
 
