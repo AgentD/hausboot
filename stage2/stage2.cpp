b6896433966a7dc574e17a1dd25245f90a499d39
David Oberhollenzer <goliath@infraroot.at>
2023-03-25 12:26:53 +0100

    Replace stage2 header with a blob that is cast to the header struct
    The information is filled in by `fatinstall` and the vbr. Previously
    the blob was initialized statically, but the compiler decided it can
    no longer do that and instead added bulky constructor code that
    nobody called. By replacing it with a blob that others fill for us,
    we gain a couple dozen bytes of wasted space back.
    
    Signed-off-by: David Oberhollenzer <goliath@infraroot.at>

--- stage2/stage2.cpp
+++ stage2/stage2.cpp
@@ -8,7 +8,7 @@
 #include "stage2.h"
 
 __attribute__ ((section(".header")))
-Stage2Info header;
+uint8_t headerBlob[sizeof(Stage2Info)];
 
 static uint16_t cylinders = 0;
 static uint16_t sectorsPerTrack = 0;
@@ -27,23 +27,25 @@ static CHSPacked LBA2CHS(uint32_t lba)
 
 void main(void *heapPtr)
 {
+	auto *header = (Stage2Info *)headerBlob;
 	TextScreen screen;
 
 	screen.Reset();
 
-	auto lba = header.BootMBREntry().StartAddressLBA();
-	auto chs = header.BootMBREntry().StartAddressCHS();
+	auto lba = header->BootMBREntry().StartAddressLBA();
+	auto chs = header->BootMBREntry().StartAddressCHS();
 
 	screen << "Hello from Stage 2!\r\n"
-	       << "    Sector count: " << header.SectorCount() << "\r\n"
+	       << "    Sector count: " << header->SectorCount() << "\r\n"
 	       << "    Boot Partition LBA: " << lba << "\r\n"
 	       << "    CHS: " << chs.Cylinder() << "/" << chs.Head() << "/"
 	       << chs.Sector() << "\r\n"
 	       << "Heap start: " << heapPtr << "\r\n";
 
-	auto disk = header.BiosBootDrive();
+	auto disk = header->BiosBootDrive();
 
-	if (!disk.ReadDriveParameters(headsPerCylinder, cylinders, sectorsPerTrack)) {
+	if (!disk.ReadDriveParameters(headsPerCylinder, cylinders,
+				      sectorsPerTrack)) {
 		screen << "Error querying disk geometry!\r\n";
 		goto fail;
 	}
