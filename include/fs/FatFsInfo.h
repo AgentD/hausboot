b52a54323b45919c5f9fa483483d2bdb4360717b
David Oberhollenzer <goliath@infraroot.at>
2023-04-02 23:41:31 +0200

    No need to use UnalignedInt in FsInfo sector
    The integers are already perfectly aligned within the sector.
    
    Signed-off-by: David Oberhollenzer <goliath@infraroot.at>

--- include/fs/FatFsInfo.h
+++ include/fs/FatFsInfo.h
@@ -7,7 +7,6 @@
 #ifndef FAT_FS_INFO_H
 #define FAT_FS_INFO_H
 
-#include "types/UnalignedInt.h"
 #include "types/ByteBlob.h"
 
 constexpr uint32_t FatFsInfoMagic1 = 0x41615252;
@@ -17,9 +16,9 @@ constexpr uint32_t FatFsInfoMagic3 = 0xAA550000;
 class FatFsInfo {
 public:
 	bool IsValid() const {
-		return _magic1.Read() == FatFsInfoMagic1 &&
-			_magic2.Read() == FatFsInfoMagic2 &&
-			_magic3.Read() == FatFsInfoMagic3;
+		return _magic1 == FatFsInfoMagic1 &&
+			_magic2 == FatFsInfoMagic2 &&
+			_magic3 == FatFsInfoMagic3;
 	}
 
 	void SetNextFreeCluster(uint32_t index) {
@@ -30,13 +29,13 @@ public:
 		_freeClusters = count;
 	}
 private:
-	UnalignedInt<uint32_t> _magic1 = FatFsInfoMagic1;
+	uint32_t _magic1 = FatFsInfoMagic1;
 	ByteBlob<480> _reserved0;
-	UnalignedInt<uint32_t> _magic2 = FatFsInfoMagic2;
-	UnalignedInt<uint32_t> _freeClusters;
-	UnalignedInt<uint32_t> _nextFreeCluster;
+	uint32_t _magic2 = FatFsInfoMagic2;
+	uint32_t _freeClusters;
+	uint32_t _nextFreeCluster;
 	ByteBlob<12> _reserved1;
-	UnalignedInt<uint32_t> _magic3 = FatFsInfoMagic3;
+	uint32_t _magic3 = FatFsInfoMagic3;
 };
 
 static_assert(sizeof(FatFsInfo) == 512);
